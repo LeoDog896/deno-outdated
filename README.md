e781514ee5456a02cb1b2952c048807798c1bd4f
Tristan <LeoDog896@hotmail.com>
2022-07-14 08:41:45 -0400

    Begin project

---
+++ README.md
@@ -0,0 +1,7 @@
+# deno-outdated
+
+Scans your project at an entry point for outdated deno dependencies.
+
+Currently works with:
+
+- deno.land/x
---
+++ cli.ts
@@ -0,0 +1,10 @@
+import { Command } from "https://deno.land/x/cliffy@v0.24.2/command/mod.ts";
+
+await new Command()
+  .name("deno-outdated")
+  .version("0.0.1")
+  .description("Check for outdated dependencies for deno.land/x and other various 3rd party vendors")
+  .action(() => {
+    console.log("Hello World!");
+  })
+  .parse(Deno.args);
