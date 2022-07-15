7ec869db63dce8c47daf8fcd3f0f1f5f3d6ff702
Tristan <LeoDog896@hotmail.com>
2022-07-15 15:10:42 -0400

    Update names

---
+++ update.ts
@@ -0,0 +1,10 @@
+import { apply } from "./regex.ts";
+import { checkRedirect } from "./redirect.ts";
+
+export async function updateOrSelf(url: string): Promise<string> {
+  return (await checkRedirect(apply(url) ?? url)) ?? Promise.resolve(url);
+}
+
+export async function update(url: string): Promise<string | undefined> {
+  return (await checkRedirect(apply(url) ?? url)) ?? undefined
+}
