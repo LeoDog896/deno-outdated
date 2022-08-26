20ff4de2efaebb021688e44522b63795b42a1831
Tristan F <LeoDog896@hotmail.com>
2022-08-25 21:08:48 -0400

    Better test to avoid full replacement

--- redirect.ts
+++ redirect.ts
@@ -8,7 +8,7 @@ export async function checkRedirect(url: string): Promise<string | undefined> {
   await response.body?.cancel();
 
   if (response.redirected) {
-    return response.url + url.endsWith("/") ? "/" : "";
+    return response.url + (url.endsWith("/") ? "/" : "");
   } else {
     return undefined;
   }
--- test/change.test.ts
+++ test/change.test.ts
@@ -24,8 +24,11 @@ const x = 'https://deno.land/std@0.146.0/testing/asserts.ts' // i-deno-outdated 
   const sourceLines = source.split("\n");
   const lines = redirect.split("\n");
 
-  assert(!lines[0].includes("%22")); // ensure that the escape character bug does not exist
-  assertNotEquals(lines[0], sourceLines[0]);
+  // ensure that the escape character bug does not exist
+  assert(!lines[0].includes("%22"));
+
+  assert(!lines[0].includes("i-deno-outdated"), "i-deno-outdated exists in the first line of the source string")
+  assertNotEquals(lines[0], sourceLines[0]); // the deno-outdated line exists only exists in the code and not the string (as proven above)
   assertEquals(lines[1], sourceLines[1]);
 
   assertNotEquals(redirect, source);
@@ -33,6 +36,15 @@ const x = 'https://deno.land/std@0.146.0/testing/asserts.ts' // i-deno-outdated 
 
 Deno.test("Slash at the end of a URL isn't removed", async () => {
   const source = `https://esm.sh/preact@10.10.6/`; // i-deno-outdated
+  const result = await findAndReplace(source);
 
-  assert((await findAndReplace(source)).endsWith("/"))
-})
+  assert(
+    result.endsWith("/"),
+    "Result doesn't end with '/': " + result,
+  );
+
+  assert(
+    result.startsWith("https"),
+    "Result doesn't start with 'https': " + result,
+  );
+});
